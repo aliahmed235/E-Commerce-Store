cd320b34fb2e225aaf66602e0c41c67872c06a69
aliahmed235 <aliahmed1174@outlook.com>
2026-04-17 13:21:26 +0200

    update

--- src/main/java/com/ali/store/repositories/OrderRepository.java
+++ src/main/java/com/ali/store/repositories/OrderRepository.java
@@ -1,7 +1,7 @@
-package com.codewithmosh.store.repositories;
+package com.ali.store.repositories;
 
-import com.codewithmosh.store.entities.Order;
-import com.codewithmosh.store.entities.User;
+import com.ali.store.entities.Order;
+import com.ali.store.entities.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
