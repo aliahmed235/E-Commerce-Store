7ae4716e7160386e2d57c948437ba1a45a47bf72
aliahmed235 <aliahmed1174@outlook.com>
2026-04-17 13:24:26 +0200

    update

--- src/main/java/com/ali/store/repositories/ProductRepository.java
+++ src/main/java/com/ali/store/repositories/ProductRepository.java
@@ -1,6 +1,6 @@
-package com.codewithmosh.store.repositories;
+package com.ali.store.repositories;
 
-import com.codewithmosh.store.entities.Product;
+import com.ali.store.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
