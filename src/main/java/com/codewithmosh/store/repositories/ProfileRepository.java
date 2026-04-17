b76710faa0ded47eeea0b9656862602dda0a5f7d
aliahmed235 <aliahmed1174@outlook.com>
2026-04-17 12:39:32 +0200

    repositories

--- src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
@@ -1,7 +1,10 @@
 package com.codewithmosh.store.repositories;
 
+import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.entities.Profile;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
 }
