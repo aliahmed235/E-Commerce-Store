a6a3a41333591c02b59842f193eb011947bf9f2e
aliahmed235 <aliahmed1174@outlook.com>
2026-04-17 12:44:23 +0200

    Data Transfer Object

---
+++ src/main/java/com/codewithmosh/store/dtos/ProductDto.java
@@ -0,0 +1,23 @@
+// java
+package com.codewithmosh.store.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDto {
+    @JsonProperty("product_id")
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private Byte categoryId;
+}
