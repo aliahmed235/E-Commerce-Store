31f2db0ebb8b451357135cdd3d6a8f68adbf179f
aliahmed235 <aliahmed1174@outlook.com>
2026-04-17 13:15:13 +0200

    update

--- src/main/java/com/ali/store/controllers/AdminController.java
+++ src/main/java/com/ali/store/controllers/AdminController.java
@@ -1,4 +1,4 @@
-package com.codewithmosh.store.controllers;
+package com.ali.store.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
