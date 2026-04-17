05911a339362e521ff46b653665eedb7d24bc2b3
aliahmed235 <aliahmed1174@outlook.com>
2026-04-17 13:26:43 +0200

    update

--- src/main/java/com/ali/store/payments/StripePaymentGateway.java
+++ src/main/java/com/ali/store/payments/StripePaymentGateway.java
@@ -1,8 +1,8 @@
-package com.codewithmosh.store.payments;
+package com.ali.store.payments;
 
-import com.codewithmosh.store.entities.Order;
-import com.codewithmosh.store.entities.OrderItem;
-import com.codewithmosh.store.entities.PaymentStatus;
+import com.ali.store.entities.Order;
+import com.ali.store.entities.OrderItem;
+import com.ali.store.entities.PaymentStatus;
 import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Event;
